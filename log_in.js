41b95b63eb98d432be034d96dc840bc9798304a7
Navas Palekkodan <pknavas1997@gmail.com>
2024-03-15 22:56:55 +0530

    first commit

---
+++ log_in.js
@@ -0,0 +1,17 @@
+  async function login() {
+    var payload = {
+      username: document.getElementById("username").value,
+      password: document.getElementById("password").value,
+    };
+    var response = await fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+
+    });
+    var data=await response.json()  
+    console.log(response,data);
+    console.log(data.token);
+  }
